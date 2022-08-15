97ab4a18c34bc3fd17284a24530e863689f4f872
moorthidaniel <moorthidaniel@gmail.com>
2022-08-15 13:26:49 +0530

    first commit

---
+++ Models/AMCDbContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace asp_mariadb_cfg.Models
+{
+    public partial class AMCDbContext : DbContext
+    {
+        public AMCDbContext()
+        {
+        }
+
+        public AMCDbContext(DbContextOptions<AMCDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Student { get; set; }
+        
+        public IConfiguration Configuration { get; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                String _connectionString = Configuration.GetConnectionString("MariaDbConnectionString");
+                optionsBuilder.UseMySql(_connectionString,  MariaDbServerVersion.AutoDetect(_connectionString));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.ToTable("student");
+
+                entity.Property(e => e.Id).HasColumnType("int(11)");
+
+                entity.Property(e => e.BirthDate).HasColumnType("date");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("utf8mb4")
+                    .UseCollation("utf8mb4_unicode_ci");
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasColumnType("varchar(30)")
+                    .HasCharSet("utf8mb4")
+                    .UseCollation("utf8mb4_unicode_ci");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
